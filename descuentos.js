8eefc919cfbddfaeb7f552a8a697ca93765eb15a
Rafael Ovidio Cordero <rafael_corderoy@hotmail.com>
2021-10-20 18:33:02 -0300

    Se incrporó el uso de HTML para capturar los datos de entrada y mostrar e resultado

--- descuentos.js
+++ descuentos.js
@@ -8,7 +8,17 @@ function calcularPorcentajePrecioConDescuento (porcentaje) {
 function calcularPrecioConDescuento (precio, descuento) {
     return (precio * calcularPorcentajePrecioConDescuento(descuento)) / 100;
 }
-console.log(precioOriginal, 
+/*console.log(precioOriginal, 
     porcentajeDescuento, 
     calcularPorcentajePrecioConDescuento (porcentajeDescuento), 
-    calcularPrecioConDescuento (precioOriginal, calcularPorcentajePrecioConDescuento (porcentajeDescuento)));
+    calcularPrecioConDescuento (precioOriginal, calcularPorcentajePrecioConDescuento (porcentajeDescuento)));*/
+
+function onClickCalculaPrecioRebajado() {
+    const inputPrice = document.getElementById ("InputPrice")
+    const price = inputPrice.value;
+    const inputDisc = document.getElementById ("InputDiscount")
+    const disccount = inputDisc.value;
+    const precioFinal = calcularPrecioConDescuento(price, disccount);
+    const resultPrice = document.getElementById("resultPrice");
+    resultPrice.innerText = "El precio con descuento es $" + precioFinal;
+}
